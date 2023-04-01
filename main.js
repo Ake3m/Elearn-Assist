c78082154759f0e5648ebe884941ad1b0469511d
Akeem Peters <akeemp13@gmail.com>
2023-04-01 14:12:03 +0800

    added temporary delete capability

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ chrome.storage.local.get(null, (assignments)=>{
     //creating html elements for the object properties
     let assignment_div=document.createElement("div");
     let assignment_div_header=document.createElement("h3");
-    //const assignment_name=document.createElement("p");
     let assignment_due_date=document.createElement("p");
     //filling the new objects with the data
     assignment_div_header.textContent=`${assignmentObj['name_of_course']}-${assignmentObj['name_of_assignment']}`;
@@ -19,11 +18,24 @@ chrome.storage.local.get(null, (assignments)=>{
     assignment_div.append(assignment_div_header);
     assignment_div.append(assignment_due_date);
     assignment_div.classList.add("assignment-container");
-
+    
+    //adding the div to the container
     upcoming_assignments.append(assignment_div);
    //adding event listener to open new window instead of using an href. On Double click, open assignment elearning page.
     assignment_div.addEventListener('dblclick',()=>{
       window.open(`${base_url}${id}`,'_blank');
+    });
+
+
+    //temporary delete functionality
+    assignment_div.addEventListener('contextmenu',(e)=>{
+      e.preventDefault();
+      if(confirm("Are you sure you want to delete this assignment?")){
+        chrome.storage.local.remove(id,()=>{
+            alert("Assignment has been removed");
+            assignment_div.style.display="none";
+        });
+      }
     });
    }
 })
